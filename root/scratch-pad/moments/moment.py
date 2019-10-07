16908e241f5bba2644de0da4a0d6e6dd9004644a
Jaak <jacques.barnard03@gmail.com>
2019-10-07 13:14:11 +0200

    13h13-07/10/2019
    die ding werk dalk nou bietjie beter.

--- root/scratch-pad/moments/moment.py
+++ root/scratch-pad/moments/moment.py
@@ -7,17 +7,32 @@ class Moment:
         self.env = env;
     
     def getMoment( self, image ):
-        if( np.shape( image )[2] == 3):
-            image = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
+        try:
+            if( np.shape( image )[2] == 3):
+                image = cv2.cvtColor( image, cv2.COLOR_BGR2GRAY )
+        except:
+            pass
         
         height, width = np.shape( image )
         y = height / 2
         x = width / 2
-        image = cv2.bitwise_not( image )
+        # image = cv2.bitwise_not( image )
+        for pixel in image:
+            pixel = abs( 255 - pixel )
+        demo = image  
+        print( image )
         for h in range( height ):
             for w in range( width ):
-                y = ( ( h / height ) * ( image[h][w] / 255 ) + h ) / 2
-                x = ( ( w / height ) * ( image[h][w] / 255 ) + w ) / 2
+                demo = image 
+                print( f' {( h / height )} x {( image[h][w] / 255 )} + {y} / 2 = {( ( h / height ) * ( image[h][w] / 255 ) + h ) / 2}')
+                y = ( ( h / height ) * ( image[h][w] / 255 ) + y ) / 2
+                x = ( ( w / height ) * ( image[h][w] / 255 ) + x ) / 2
+                print( f'y:{y} x:{x}' )
+
+                demo[int(round(y))][int(round(x))] = 255
+                cv2.imshow( 'after', image )
+                cv2.waitKey( 0 )
+                cv2.destroyAllWindows()
 
 
             print({
@@ -27,9 +42,6 @@ class Moment:
 
         image[int(round(y))][int(round(x))] = 255;
         
-        cv2.imshow( 'before', image )
-        cv2.waitKey( 0 )
-        cv2.destroyAllWindows()
         return self.env['parameters']['moments']['msg']
 
 
@@ -42,6 +54,17 @@ if __name__ == "__main__":
         }
     }
 
-    image = cv2.imread('images/0050-1_102.tif')
+    # image = cv2.imread('images/0050-1_102.tif')
+
+    image = [[255, 255, 0, 255, 255],
+             [255, 255, 255, 255, 255],
+             [255, 255, 255, 255, 255],
+             [255, 255, 255, 255, 255],
+             [255, 255, 255, 255, 255]];
+    image = cv2.cvtColor(np.array( Image.fromarray( np.asfarray( image )) ), cv2.COLOR_RGB2BGR)
+    
+    cv2.imshow( 'before', image )
+    cv2.waitKey( 0 )
+    cv2.destroyAllWindows()
 
     print( Moment( env ).getMoment( image ) )
