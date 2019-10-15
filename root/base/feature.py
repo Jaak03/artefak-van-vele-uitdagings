0a080a35da2cd6cae3d2c72b9837c7a77e8682a6
Jaak <jacques.barnard03@gmail.com>
2019-10-15 12:05:09 +0200

    12h04-15/10/2019
    ek is hier besig om aan die pyplyn vir die features te werk.

--- root/base/feature.py
+++ root/base/feature.py
@@ -6,6 +6,7 @@ from base.message_bucket import TestMessage
 
 class FeaturePipeline:
     def __init__( self, env, path = None ):
+        state( 'Setting up the image pipeline.' )
         self._e = env
 
         if( path != None ):
@@ -13,14 +14,22 @@ class FeaturePipeline:
 
                 # Inside each sample folder there are images for lines and words, which are folders.
                 subject = ask( 'Would you like to use [words] or [lines]?' )
-                while( not(subject == 'words' or subject == 'lines' )):
+                while( not(subject.payload == 'words' or subject.payload == 'lines' )):
                     subject = ask( 'Type either [words] or [lines]?' )
 
-                
-                print( self._directory_list )
+                self.setPaths( subject.payload )
 
-    
+    # def getPaths( ):
+    def setPaths( self, subject: str ):
+        comment( 'Setting paths for image directories.' )
+
+        for i in range( len(self._directory_list) ):
+            self._directory_list[ i ] = f'{ self._directory_list[ i ] }/{ subject }'
+        print( self._directory_list )
+
+
     def getDirectories( self, path ):
+        comment( 'Reading directories in dataset.' )
         try:
             samples = []
             for dir in os.scandir( path ):
@@ -48,7 +57,8 @@ if __name__ == "__main__":
     e = Environment( out, os.getcwd() )
 
     dataset = out.ask( 'What dataset would you like to use?' )
-    if( dataset.success ):
+    # if( dataset.success ):
+    if( True ):    
         try:
             for key in e.paths.content.keys():
                 if( key == dataset.payload ):
